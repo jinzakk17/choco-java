637c9504177ee0b5fc3fb4390c267acbcff845df
Adrian Eusebio <adrian.eusebio17@gmail.com>
2016-04-26 15:12:09 +0800

    Fix Ping and Astroport

--- src/main/java/yose/YoseGame.java
+++ src/main/java/yose/YoseGame.java
@@ -23,12 +23,6 @@ public class YoseGame {
         }, new FreeMarkerEngine());
     	
     	get("/ping", (request, response) -> {
-    		response.type("text/html");
-    		
-            return new ModelAndView(new Object(), "ping.ftl");
-        }, new FreeMarkerEngine());
-    	
-    	get("/ping", (request, response) -> {
     		response.type("application/json");
     		
     		return new Status(true); 
